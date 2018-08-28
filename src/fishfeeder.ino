4ad9a222044ae724539c234325fe23395dbf70b1
Toby Kurien <toby@tobykurien.com>
2018-08-28 16:25:45 +0200

    wip

--- src/fishfeeder.ino
+++ src/fishfeeder.ino
@@ -27,10 +27,9 @@ void setup() {
     if (rtc.lostPower()) {
         Serial.println("RTC lost power, lets set the time!");
         // following line sets the RTC to the date & time this sketch was compiled
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
         // This line sets the RTC with an explicit date & time, for example to set
-        // January 21, 2014 at 3am you would call:
-        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+        //rtc.adjust(DateTime(2018, 8, 28, 16, 16, 0));
     }
 
     servo.attach(SERVO);
