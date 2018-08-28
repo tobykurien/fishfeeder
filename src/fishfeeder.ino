3e563f01865d8d5ce715cbabd0c6500c3bd17c04
Toby Kurien <toby@tobykurien.com>
2018-08-28 15:34:12 +0200

    new wemos implementation with RTC and thermistor

--- src/fishfeeder.ino
+++ src/fishfeeder.ino
@@ -1,32 +1,48 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <RTClib.h>
 
-#define SERVO   3
-#define LED     12
-
-unsigned long millisHour = 3600000; // adjust for clock drift. 3600000 in a perfect world.
-unsigned long startMillis = 0;
+#define TEMPERATURE A0
+#define SERVO       2
+#define LED         12
 
 Servo servo;
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {
+    "Sunday", "Monday", "Tuesday", "Wednesday", 
+    "Thursday", "Friday", "Saturday"
+};
 
 void setup() {
     Serial.begin(9600);
     Serial.println("Starting fish feeder");
 
+    if (!rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        while (1);
+    }
+
+    if (rtc.lostPower()) {
+        Serial.println("RTC lost power, lets set the time!");
+        // following line sets the RTC to the date & time this sketch was compiled
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        // This line sets the RTC with an explicit date & time, for example to set
+        // January 21, 2014 at 3am you would call:
+        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    }
+
     servo.attach(SERVO);
     servo.write(0);
     pinMode(LED, OUTPUT);
-
-    // start timing from now
-    startMillis = millis();
 }
 
 void loop() {
-    delayHours(24, startMillis);
-
-    // feed once a day for winter
-    startMillis = millis();
-    dumpFood();
+    Serial.println(temperature());
+    showTime();
+    servo.write(90);
+    delay(1000);
+    servo.write(0);
 }
 
 void dumpFood() {
@@ -54,39 +70,39 @@ void dumpFood() {
     servo.write(0);
 }
 
-void delayHours(unsigned int hours, unsigned long start) {
-    Serial.print("Delaying for hours ");
-    Serial.println(hours);
+int temperature() {
+    int t = analogRead(TEMPERATURE);
+    //Serial.println(t);
 
-    unsigned long totalMillis = hours * millisHour;
-    for (int hour=hours; hour > 0; hour--) {
-        Serial.print("Starting hour ");
-        Serial.println(hour);
-
-        while ((unsigned long)(millis() - start) < millisHour) {
-            if (hour > 5) {
-                delay(5000);
-            } else if (hour > 1) {
-                delay(1000 * hour);
-            } else {
-                if ((millis() - start) < 1800000) {
-                    // more than 30 mins to go
-                    delay(750);
-                } else if ((millis() - start) < 3000000) {
-                    // more than 10 minutes to go
-                    delay(500);
-                } else if ((millis() - start) < 3540000) {
-                    // more than 1 minute to go
-                    delay(250);
-                } else {
-                    delay(100);
-                }
-            }
-
-            digitalWrite(LED, HIGH);
-            delay(100);
-            digitalWrite(LED, LOW);
-        }
-    } 
+    // calibrated to degrees celcius with thermometer
+    int reading = map(t, 255, 462, -1, 20);
+    return reading;
 }
 
+void showTime() {
+    DateTime now = rtc.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    // Serial.print(" since midnight 1/1/1970 = ");
+    // Serial.print(now.unixtime());
+    // Serial.print("s = ");
+    // Serial.print(now.unixtime() / 86400L);
+    // Serial.println("d");   
+
+    // calculate a date which is 7 days and 30 seconds into the future
+    // DateTime future (now + TimeSpan(7,12,30,6));     
+}
